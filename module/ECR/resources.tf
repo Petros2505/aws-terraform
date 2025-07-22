75323764d7e235ce7631ee22c5b0669a88f4ef55
Petros2505 <pethov25@gmail.com>
2025-07-22 13:53:26 +0400

    commit

--- module/ECR/resources.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_ecr_repository" "app" {
-  name = var.ecr_repository
-
-  tags = merge(var.tags, {
-    Name = var.ecr_repository
-
-  })
-}
--- module/ECR/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "tags" {
-  type    = map(string)
-  default = {}
-}
-variable "ecr_repository" {
-  type    = string
-  default = "ecr-repo"
-}
